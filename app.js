d6511a115f5d9aab730a8a15d07c91fff563699b
iwebsteraudio <iwebster.audio@gmail.com>
2024-07-10 13:29:40 +0100

    successfully built nodemailer

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+require("dotenv").config({ path: "./.env.test" });
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const emailRoutes = require("./routes/emailRoutes");
+
 const app = express();
-const { sendUserData, postUser } = require("./controllers/users-controllers");
-const { sendSongsData, sendCustom404 } = require("./controllers/songs-controllers");
 const cors = require("cors");
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use("/api", emailRoutes);
+
+const { sendUserData, postUser } = require("./controllers/users-controllers");
+const {
+  sendSongsData,
+  sendCustom404,
+} = require("./controllers/songs-controllers");
 
 app.get("/api/users", sendUserData);
-
 app.post("/api/users", postUser);
-
 app.get("/api/songs", sendSongsData);
 
-// need to be able to get song data, user data, post a user, post a song
-
 app.use(express.json());
-
 app.all("*", sendCustom404);
 
 app.use((err, req, res, next) => {
@@ -27,11 +33,13 @@ app.use((err, req, res, next) => {
     err.code === "23503"
   ) {
     res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
   }
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).send({ msg: "Internal Server Error" });
+  res.status(500).send({ msg: "Internal Server Error: Bleeding organs" });
 });
 
 module.exports = app;
---
+++ controllers/email-controllers.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config({ path: "./.env.test" });
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+exports.sendEmail = async (req, res) => {
+  const { name, contactNumber, contactEmail, message } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: `Contact Form Submission from ${name}`,
+    text: `Name: ${name}\nContact Number: ${contactNumber}\nContact Email: ${contactEmail}\n${message}`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send({ message: "Email sent successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Error sending email", error });
+  }
+};
